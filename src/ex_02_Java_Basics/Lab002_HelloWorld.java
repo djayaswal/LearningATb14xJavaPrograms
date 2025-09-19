d1495c17a14d2c7b09d4c4ea7a7d43aef2496948
djayaswal <djayaswa@gmail.com>
2025-09-19 18:04:32 +0530

    Adding third Push from Lab 002

---
+++ src/ex_02_Java_Basics/Lab002_HelloWorld.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab002_HelloWorld {
+
+        static public void main(String args[])
+        {
+            System.out.println("Hello World");
+        }
+    }
+
